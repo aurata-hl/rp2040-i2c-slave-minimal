faaed85f55f8ba7e771cf24ac35aac3814c6a3a3
Henrik Lauritzen <henrik.lauritzen@aurata.com>
2025-04-10 13:19:49 +0200

    Extract example code from comments into its own file.
    test_i2c_slave.py is a new module containing the original example code,
    wrapped as a function with arguments.

---
+++ test_i2c_slave.py
@@ -0,0 +1,56 @@
+def test_slave(addr, bus=0, sda=0, scl=1):
+    """
+    Example code demonstrating the use of RP2040_Slave.
+    It creates a simple slave on the specified `addr` and
+    RP2040 `bus`, using the GPIO pins `sda` and `scl`.
+    """
+    from RP2040_Slave import i2c_slave
+
+    # Initialize an empty buffer list for sequential write sequences
+
+    data_buf = []
+    s_i2c = i2c_slave(bus, sda=sda, scl=scl, slaveAddress=addr)
+    state = s_i2c.I2CStateMachine.I2C_START
+    currentTransaction = s_i2c.I2CTransaction(addr, data_buf)
+    counter = 0
+
+    print(f"I2C Slave test, address {addr:02x}, bus {bus}, pins {sda}:{scl}")
+    try:
+        while True:
+            state = s_i2c.handle_event()
+
+            if state == s_i2c.I2CStateMachine.I2C_START:
+                pass
+
+            if state == s_i2c.I2CStateMachine.I2C_RECEIVE:
+                if currentTransaction.address == 0x00:
+                    # First byte received is the register address
+                    currentTransaction.address = s_i2c.Read_Data_Received()
+
+                # Read all data byte received until RX FIFO is empty
+                while (s_i2c.Available()):
+                    currentTransaction.data_byte.append(
+                        s_i2c.Read_Data_Received())
+                    # Virtually Increase register address
+                    # s_i2c.I2CTransaction.address += 1
+
+            if state == s_i2c.I2CStateMachine.I2C_REQUEST:
+                # Send some dummy data back
+                while (s_i2c.is_Master_Req_Read()):
+                    counter += 1
+                    s_i2c.Slave_Write_Data(counter)
+
+                    # Virtually Increase register address
+                    # s_i2c.I2CTransaction.address += 1
+                    print("Sending data : ", counter)
+
+            if state == s_i2c.I2CStateMachine.I2C_FINISH:
+                print(
+                    "Register : ", currentTransaction.address,
+                    "Received : ", currentTransaction.data_byte)
+
+                currentTransaction.address = 0x00
+                currentTransaction.data_byte = []
+
+    except KeyboardInterrupt:
+        pass
