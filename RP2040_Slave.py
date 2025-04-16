89100b5e92aba3604e835d91274321a2c7f7f2a2
Henrik Lauritzen <henrik.lauritzen@aurata.com>
2025-04-16 23:27:54 +0200

    RP2040_Slave.py Reduce overhead by eliminating lookup tables.
    Reduces code size and memory use while speeding up the poll
    routine and reducing the risk of GC sycles, too.

--- RP2040_Slave.py
+++ RP2040_Slave.py
@@ -2,12 +2,46 @@
 
 from machine import mem32
 from micropython import const
-from RP2040_I2C_Registers import *
 
-# I2C register addresses.
+
+# RP2040 I2C register base addresses
 _I2C0_BASE = const(0x40044000)
 _I2C1_BASE = const(0x40048000)
 _IO_BANK0_BASE = const(0x40014000)
+
+# RP2040 I2C register relative addresses
+_I2C_IC_CLR_RD_REQ = const(0x00000050)
+_I2C_IC_CLR_RESTART_DET = const(0x000000a8)
+_I2C_IC_CLR_RX_DONE = const(0x00000058)
+_I2C_IC_CLR_START_DET = const(0x00000064)
+_I2C_IC_CLR_STOP_DET = const(0x00000060)
+_I2C_IC_CLR_TX_ABRT = const(0x00000054)
+_I2C_IC_CON = const(0x00000000)
+_I2C_IC_DATA_CMD = const(0x00000010)
+_I2C_IC_ENABLE = const(0x0000006c)
+_I2C_IC_INTR_STAT = const(0x0000002c)
+_I2C_IC_RAW_INTR_STAT = const(0x00000034)
+_I2C_IC_SAR = const(0x00000008)
+_I2C_IC_STATUS = const(0x00000070)
+
+# RP2040 I2C register bitmasks
+_I2C_IC_CON__IC_SLAVE_DISABLE = const(0x00000040)
+_I2C_IC_CON__MASTER_MODE = const(0x00000001)
+_I2C_IC_CON__RX_FIFO_FULL_HLD_CTRL = const(0x00000200)
+_I2C_IC_CON__STOP_DET_IFADDRESSED = const(0x00000080)
+_I2C_IC_DATA_CMD__DAT = const(0x000000ff)
+_I2C_IC_DATA_CMD__FIRST_DATA_BYTE = const(0x00000800)
+_I2C_IC_ENABLE__ENABLE = const(0x00000001)
+_I2C_IC_INTR_STAT__R_RD_REQ = const(0x00000020)
+_I2C_IC_INTR_STAT__R_RESTART_DET = const(0x00001000)
+_I2C_IC_INTR_STAT__R_RX_DONE = const(0x00000080)
+_I2C_IC_INTR_STAT__R_START_DET = const(0x00000400)
+_I2C_IC_INTR_STAT__R_STOP_DET = const(0x00000200)
+_I2C_IC_INTR_STAT__R_TX_ABRT = const(0x00000040)
+_I2C_IC_RAW_INTR_STAT__RD_REQ = const(0x00000020)
+_I2C_IC_SAR__IC_SAR = const(0x000003ff)
+_I2C_IC_STATUS__RFNE = const(0x00000008)
+
 
 # Atomic Register Access
 MEM_RW = const(0x0000)  # Normal read/write access
@@ -61,18 +95,15 @@ class i2c_slave:
 
         # 1. Disable the DW_apb_i2c by writing a ‘0’ to IC_ENABLE.ENABLE
         self.RP2040_Clear_32b_i2c_Reg(
-            I2C_OFFSET["I2C_IC_ENABLE"],
-            self.get_Bits_Mask("ENABLE", I2C_IC_ENABLE))
+            _I2C_IC_ENABLE, _I2C_IC_ENABLE__ENABLE)
 
         # 2. Write to the IC_SAR register (bits 9:0) to set the slave address.
         # This is the address to which the DW_apb_i2c responds.
         self.RP2040_Clear_32b_i2c_Reg(
-            I2C_OFFSET["I2C_IC_SAR"],
-            self.get_Bits_Mask("IC_SAR", I2C_IC_SAR))
+            _I2C_IC_SAR, _I2C_IC_SAR__IC_SAR)
 
         self.RP2040_Set_32b_i2c_Reg(
-            I2C_OFFSET["I2C_IC_SAR"],
-            self.slaveAddress & self.get_Bits_Mask("IC_SAR", I2C_IC_SAR))
+            _I2C_IC_SAR, self.slaveAddress & _I2C_IC_SAR__IC_SAR)
 
         # 3. Write to the IC_CON register to specify which type of addressing
         # is supported (7-bit or 10-bit by setting bit 3).
@@ -82,23 +113,19 @@ class i2c_slave:
 
         # Disable Master mode
         self.RP2040_Clear_32b_i2c_Reg(
-            I2C_OFFSET["I2C_IC_CON"],
-            self.get_Bits_Mask("MASTER_MODE", I2C_IC_CON))
+            _I2C_IC_CON, _I2C_IC_CON__MASTER_MODE)
 
         # Enable slave mode
         self.RP2040_Clear_32b_i2c_Reg(
-            I2C_OFFSET["I2C_IC_CON"],
-            self.get_Bits_Mask("IC_SLAVE_DISABLE", I2C_IC_CON))
+            _I2C_IC_CON, _I2C_IC_CON__IC_SLAVE_DISABLE)
 
         # Enable clock strech
         self.RP2040_Set_32b_i2c_Reg(
-            I2C_OFFSET["I2C_IC_CON"],
-            self.get_Bits_Mask("RX_FIFO_FULL_HLD_CTRL", I2C_IC_CON))
+            _I2C_IC_CON, _I2C_IC_CON__RX_FIFO_FULL_HLD_CTRL)
 
         # 4. Enable the DW_apb_i2c by writing a ‘1’ to IC_ENABLE.ENABLE.
         self.RP2040_Set_32b_i2c_Reg(
-            I2C_OFFSET["I2C_IC_ENABLE"],
-            self.get_Bits_Mask("IC_ENABLE", I2C_IC_ENABLE))
+            _I2C_IC_ENABLE, _I2C_IC_ENABLE__ENABLE)
 
         # Reset GPIO0 function
         sda_base = 4 + 8 * self.sda
@@ -134,54 +161,46 @@ class i2c_slave:
     def handle_event(self):
         # I2C Master has abort the transactions
         if (self.RP2040_Get_32b_i2c_Bits(
-                I2C_OFFSET["I2C_IC_INTR_STAT"],
-                self.get_Bits_Mask("R_TX_ABRT", I2C_IC_INTR_STAT))):
+                _I2C_IC_INTR_STAT, _I2C_IC_INTR_STAT__R_TX_ABRT)):
             # Clear int
-            self.RP2040_Read_32b_i2c_Reg(I2C_OFFSET["I2C_IC_CLR_TX_ABRT"])
+            self.RP2040_Read_32b_i2c_Reg(_I2C_IC_CLR_TX_ABRT)
             return i2c_slave.I2CStateMachine.I2C_FINISH
 
         # Last byte transmitted by I2C Slave but NACK from I2C Master
         if (self.RP2040_Get_32b_i2c_Bits(
-                I2C_OFFSET["I2C_IC_INTR_STAT"],
-                self.get_Bits_Mask("R_RX_DONE", I2C_IC_INTR_STAT))):
+                _I2C_IC_INTR_STAT, _I2C_IC_INTR_STAT__R_RX_DONE)):
             # Clear int
-            self.RP2040_Read_32b_i2c_Reg(I2C_OFFSET["I2C_IC_CLR_RX_DONE"])
+            self.RP2040_Read_32b_i2c_Reg(_I2C_IC_CLR_RX_DONE)
             return i2c_slave.I2CStateMachine.I2C_FINISH
 
         # Restart condition detected
         if (self.RP2040_Get_32b_i2c_Bits(
-                I2C_OFFSET["I2C_IC_INTR_STAT"],
-                self.get_Bits_Mask("R_RESTART_DET", I2C_IC_INTR_STAT))):
+                _I2C_IC_INTR_STAT, _I2C_IC_INTR_STAT__R_RESTART_DET)):
             # Clear int
-            self.RP2040_Read_32b_i2c_Reg(I2C_OFFSET["I2C_IC_CLR_RESTART_DET"])
+            self.RP2040_Read_32b_i2c_Reg(_I2C_IC_CLR_RESTART_DET)
 
         # Start condition detected by I2C Slave
         if (self.RP2040_Get_32b_i2c_Bits(
-                I2C_OFFSET["I2C_IC_INTR_STAT"],
-                self.get_Bits_Mask("R_START_DET", I2C_IC_INTR_STAT))):
+                _I2C_IC_INTR_STAT, _I2C_IC_INTR_STAT__R_START_DET)):
             # Clear start detection
-            self.RP2040_Read_32b_i2c_Reg(I2C_OFFSET["I2C_IC_CLR_START_DET"])
+            self.RP2040_Read_32b_i2c_Reg(_I2C_IC_CLR_START_DET)
             return i2c_slave.I2CStateMachine.I2C_START
 
         # Stop condition detected by I2C Slave
         if (self.RP2040_Get_32b_i2c_Bits(
-                I2C_OFFSET["I2C_IC_INTR_STAT"],
-                self.get_Bits_Mask("R_STOP_DET", I2C_IC_INTR_STAT))):
-
+                _I2C_IC_INTR_STAT, _I2C_IC_INTR_STAT__R_STOP_DET)):
             # Clear stop detection
-            self.RP2040_Read_32b_i2c_Reg(I2C_OFFSET["I2C_IC_CLR_STOP_DET"])
+            self.RP2040_Read_32b_i2c_Reg(_I2C_IC_CLR_STOP_DET)
             return i2c_slave.I2CStateMachine.I2C_FINISH
 
         # Check if RX FIFO is not empty
         if (self.RP2040_Get_32b_i2c_Bits(
-                I2C_OFFSET["I2C_IC_STATUS"],
-                self.get_Bits_Mask("RFNE", I2C_IC_STATUS))):
+                _I2C_IC_STATUS, _I2C_IC_STATUS__RFNE)):
             return i2c_slave.I2CStateMachine.I2C_RECEIVE
 
         # Check if Master is requesting data
         if (self.RP2040_Get_32b_i2c_Bits(
-                I2C_OFFSET["I2C_IC_INTR_STAT"],
-                self.get_Bits_Mask("R_RD_REQ", I2C_IC_INTR_STAT))):
+                _I2C_IC_INTR_STAT, _I2C_IC_INTR_STAT__R_RD_REQ)):
             # Shall Wait until transfer is done, timing recommended
             # 10 * fastest SCL clock period:
             # for 100 Khz = (1/100E3) * 10 = 100 uS
@@ -192,8 +211,7 @@ class i2c_slave:
         """ Return status if I2C Master is requesting a read sequence """
         # Check RD_REQ Interrupt bit (master wants to read data from the slave)
         status = self.RP2040_Get_32b_i2c_Bits(
-            I2C_OFFSET["I2C_IC_RAW_INTR_STAT"],
-            self.get_Bits_Mask("RD_REQ", I2C_IC_RAW_INTR_STAT))
+            _I2C_IC_RAW_INTR_STAT, _I2C_IC_RAW_INTR_STAT__RD_REQ)
         return bool(status)
 
     """
@@ -202,21 +220,19 @@ class i2c_slave:
         #
         # Check whether is FIRST_DATA_BYTE bit is active in IC_DATA_CMD.
         first_data_byte_stat = self.RP2040_Get_32b_i2c_Bits(
-            I2C_OFFSET["I2C_IC_DATA_CMD"],
-            self.get_Bits_Mask("FIRST_DATA_BYTE", I2C_IC_DATA_CMD))
+            _I2C_IC_DATA_CMD, _I2C_IC_DATA_CMD__FIRST_DATA_BYTE)
 
         # Check whether is STOP_DET_IFADDRESSED bit is active in IC_CON.
         stop_stat = self.RP2040_Get_32b_i2c_Bits(
-            I2C_OFFSET["I2C_IC_CON"],
-            self.get_Bits_Mask("STOP_DET_IFADDRESSED", I2C_IC_CON))
+            _I2C_IC_CON, _I2C_IC_CON__STOP_DET_IFADDRESSED)
 
         if (stop_stat):
             # Clear stop bit int
-            self.RP2040_Read_32b_i2c_Reg(I2C_OFFSET["I2C_IC_CLR_STOP_DET"])
+            self.RP2040_Read_32b_i2c_Reg(_I2C_IC_CLR_STOP_DET)
 
         # Master sequential write true if FIRST_DATA_BYTE bit is active
         # and no stop condition detected.
-        if first_data_byte_stat and not(stop_stat):
+        if first_data_byte_stat and not stop_stat:
             return True
         return False
     """
@@ -226,21 +242,18 @@ class i2c_slave:
 
         # Send data
         self.RP2040_Write_32b_i2c_Reg(
-            I2C_OFFSET["I2C_IC_DATA_CMD"],
-            data & self.get_Bits_Mask("DAT", I2C_IC_DATA_CMD))
+            _I2C_IC_DATA_CMD, data & _I2C_IC_DATA_CMD__DAT)
 
-        self.RP2040_Read_32b_i2c_Reg(I2C_OFFSET["I2C_IC_CLR_RD_REQ"])
+        self.RP2040_Read_32b_i2c_Reg(_I2C_IC_CLR_RD_REQ)
 
     def Available(self):
         """ Return true if data has been received from I2C Master """
 
         # Get RFNE Bit (Receive FIFO Not Empty)
         return self.RP2040_Get_32b_i2c_Bits(
-            I2C_OFFSET["I2C_IC_STATUS"],
-            self.get_Bits_Mask("RFNE", I2C_IC_STATUS))
+            _I2C_IC_STATUS, _I2C_IC_STATUS__RFNE)
 
     def Read_Data_Received(self):
         """ Return data from I2C Master """
         return self.RP2040_Read_32b_i2c_Reg(
-            I2C_OFFSET["I2C_IC_DATA_CMD"]
-        ) & self.get_Bits_Mask("DAT", I2C_IC_DATA_CMD)
+            _I2C_IC_DATA_CMD) & _I2C_IC_DATA_CMD__DAT
